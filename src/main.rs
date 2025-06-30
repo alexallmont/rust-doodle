f04d10313d8c89051c6fd9b780a9968deb239589
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2025-06-30 22:06:13 +0100

    refactor: trait for diffsol matrix to py

--- src/main.rs
+++ src/main.rs
@@ -5,50 +5,45 @@ use numpy::ndarray::ShapeBuilder; // Required for strides() on shape
 use numpy::{PyArray2, ToPyArray};
 use pyo3::prelude::*;
 
+pub trait MatrixToPy<'py> {
+    fn to_pyarray_view(&self, py: Python<'py>) -> Bound<'py, PyArray2<f64>>;
+}
+
+impl<'py> MatrixToPy<'py> for DMatrix<f64> {
+    fn to_pyarray_view(&self, py: Python<'py>) -> Bound<'py, PyArray2<f64>> {
+        let view = unsafe {
+            ArrayView2::from_shape_ptr(
+                self.shape().strides(self.strides()),
+                self.as_ptr()
+            )
+        };
+        view.to_pyarray(py).into()
+    }
+}
+
+impl<'py> MatrixToPy<'py> for Mat<f64> {
+    fn to_pyarray_view(&self, py: Python<'py>) -> Bound<'py, PyArray2<f64>> {
+        let strides = (self.row_stride() as usize, self.col_stride() as usize);
+        let view = unsafe {
+            ArrayView2::from_shape_ptr(
+                self.shape().strides(strides),
+                self.as_ptr()
+            )
+        };
+        view.to_pyarray(py).into()
+    }
+}
+
 fn nalgebra_test<'py>(py: Python<'py>) -> Bound<'py, PyArray2<f64>>{
     let mut mat: DMatrix<f64> = DMatrix::zeros(7, 13);
     mat[(2, 5)] = 5.0; // Example value to test orientation
-
-    println!("nrows: {}", mat.nrows());
-    println!("ncols: {}", mat.ncols());
-
-    let shape = mat.shape();
-    let strides = mat.strides();
-    println!("shape: {:?}", shape);
-    println!("strides: {:?}", strides);
-
-    let ptr = mat.as_ptr();
-    let view = unsafe {
-        ArrayView2::from_shape_ptr(
-            shape.strides(strides),
-            ptr
-        )
-    };
-
-    view.to_pyarray(py).into()
+    mat.to_pyarray_view(py)
 }
 
-fn faer_test<'py>(py: Python<'py>) -> Bound<'py, PyArray2<f64>> {
+fn faer_test<'py>(py: Python<'py>) -> Bound<'py, PyArray2<f64>>{
     let mut mat: Mat<f64> = Mat::zeros(7, 13);
     *mat.get_mut(2, 5) = 5.0; // Example value to test orientation
-
-    println!("nrows: {}", mat.nrows());
-    println!("ncols: {}", mat.ncols());
-
-    let shape = mat.shape();
-    let strides = (mat.row_stride() as usize, mat.col_stride() as usize);
-    println!("shape: {:?}", shape);
-    println!("strides: {:?}", strides);
-
-    let ptr = mat.as_ptr();
-    let view = unsafe {
-        ArrayView2::from_shape_ptr(
-            shape.strides(strides),
-            ptr
-        )
-    };
-
-    view.to_pyarray(py).into()
+    mat.to_pyarray_view(py)
 }
 
 // TODO
@@ -64,7 +59,6 @@ fn faer_test<'py>(py: Python<'py>) -> Bound<'py, PyArray2<f64>> {
 //    `cargo +nightly miri run`
 // 3. General outstanding work:
 //  - Lifetime of data (ensure objects that created arrays are kept in scope/PyCell/problem)
-//  - Use traits to avoid duplication in nalgebra/faer examples above.
 fn main() -> PyResult<()> {
     Python::with_gil(|py| {
         let sys = py.import("sys").unwrap();
